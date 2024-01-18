ada4fd3e962ad1372eeabbeb5901e801daf8b958
Pallavi Goel <pallavigoel2610@gmail.com>
2024-01-18 10:37:39 +0000

    add complete order recipt

---
+++ checkout/__init__.py
@@ -0,0 +1,2 @@
+default_app_config = 'checkout.apps.CheckoutConfig' 
+#without tis line in the innit file, django wouldn't know about our custom ready method about our signals wouldn't work.
--- checkout/static/checkout/css/js/stripe_elements.js
+++ checkout/static/checkout/css/js/stripe_elements.js
@@ -6,9 +6,9 @@
     https://stripe.com/docs/stripe-js
 */
 
-var stripe_public_key =  $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
     base: {
@@ -29,13 +29,13 @@ var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
 // Handle realtime validation errors on the card element
-card.addEventListener('change', function (event){
-    var errorDiv = documnent.getElementById('car-errors');
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
     if (event.error) {
         var html = `
             <span class="icon" role="alert">
-                <i class = "fas fa-times"></i>
-            </span> 
+                <i class="fas fa-times"></i>
+            </span>
             <span>${event.error.message}</span>
         `;
         $(errorDiv).html(html);
@@ -51,6 +51,8 @@ form.addEventListener('submit', function(ev) {
     ev.preventDefault();
     card.update({ 'disabled': true});
     $('#submit-button').attr('disabled', true);
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
@@ -64,6 +66,8 @@ form.addEventListener('submit', function(ev) {
                 </span>
                 <span>${result.error.message}</span>`;
             $(errorDiv).html(html);
+            $('#payment-form').fadeToggle(100);
+            $('#loading-overlay').fadeToggle(100);
             card.update({ 'disabled': false});
             $('#submit-button').attr('disabled', false);
         } else {
