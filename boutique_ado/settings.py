445ac37ceef8c114300067b85109288424e784b0
Pallavi Goel <pallavigoel2610@gmail.com>
2024-01-24 11:14:02 +0000

    deploy5

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,11 +27,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
-ALLOWED_HOSTS = ['bproject-a02974c4fb13.herokuapp.com', 'localhost']
-#ALLOWED_HOSTS = ['8000-pallavigoel-boutiprojec-fqu5ugdfzl6.ws-eu107.gitpod.io']
+#ALLOWED_HOSTS = ['bproject-a02974c4fb13.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-pallavigoel-boutiprojec-fqu5ugdfzl6.ws-eu107.gitpod.io']
 CSRF_TRUSTED_ORIGINS = ['https://8000-pallavigoel-boutiprojec-fqu5ugdfzl6.ws-eu107.gitpod.io']
 
 
