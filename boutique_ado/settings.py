ed65790bf9959b15310335a12bed320b1718be92
Pallavi Goel <pallavigoel2610@gmail.com>
2024-01-24 10:59:57 +0000

    change requirements.txt

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['bproject-a02974c4fb13.herokuapp.com', 'localhost']
 #ALLOWED_HOSTS = ['8000-pallavigoel-boutiprojec-fqu5ugdfzl6.ws-eu107.gitpod.io']
