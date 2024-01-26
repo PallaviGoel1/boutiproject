d4778907d5f39a19842b0919fd5e804ec9298f36
Pallavi Goel <pallavigoel2610@gmail.com>
2024-01-26 14:57:42 +0000

    13 deployment part5

---
+++ env.py
@@ -0,0 +1,11 @@
+import os
+
+os.environ["SECRET_KEY"] = "ykpgusnrpxivyqxs7optt75_b&iw48(@do6k+u4mpyg)*5(!*r"
+os.environ["USE_AWS"] = "True"
+os.environ["AWS_ACCESS_KEY_ID"] = "AKIATCKATXXZDKW6RXTJ"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "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"
+os.environ["DATABASE_URL"] = "postgres://vrofanmx:Gd4txRhHMzg4m5cByjiZcRJQ3VOBcmCj@manny.db.elephantsql.com/vrofanmx"
+os.environ["STRIPE_PUBLIC_KEY"] = "pk_test_51OZVcdKjqt1ZoBTg3rmOzIfb7psOjnlRY2jiG4MGBa881RS3bdGyciwt6sl3LvfGIrev166baXuN1EhH"
+os.environ["STRIPE_SECRET_KEY"] = "sk_test_51OZVcdKjqt1ZoBTgGu3ZLwzdo7iix5G7s9zCTG4zGhaKnCWIrIRB5GyqtQywqhDefZhcPWEUTkY0"
+os.environ["STRIPE_WH_SECRET"] = "whsec_Vgi5j0qZBgvweZM63m0kSyQyZTlk6Gjr"
+os.environ["AWS_DEFAULT_ACL"] = "None"
